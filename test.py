7aafaa8a4347fb04df346ed02287efe8d5d5f7f3
APPLE <ilemobayosamson@gmail.com>
2021-04-12 18:32:51 +0100

    test script

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-from monnify.monnify import *
+# from monnify.monnify import *
 
 
-mon = Monnify()
+# mon = Monnify()
 
-baseurl = "https://sandbox.monnify.com"
-username = "MK_TEST_AZD4Y6BYMS"
-password = "BZTCB3FV9F9MBT3G5687JCBKM8PSU68X"
+# baseurl = "https://sandbox.monnify.com"
+# username = "MK_TEST_AZD4Y6BYMS"
+# password = "BZTCB3FV9F9MBT3G5687JCBKM8PSU68X"
 
 
-token = Authenticate(username, password, baseurl)
-print(token)
+# token = Authenticate(username, password, baseurl)
+# print(token)
